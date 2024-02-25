b86a541334e65e3911763c23727054c67bb22510
praveensurya <praveensd@tekion.com>
2024-02-25 11:01:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let imgbox = document.getElementById("imgbox");
+let qrimage = document.getElementById("qrimage");
+let qrText = document.getElementById("qrText");
+function generateQr(){
+    if (qrText.value.length>0){
+    qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrText.value;
+    imgbox.classList.add("show-img");}
+    else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+            qrText.classList.add('error');
+        },1000);
+    }
+}
